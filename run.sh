46e7aae778502631b1a803a0b3238fd5791981ba
Inom Mirzaev <mirzaevinom@gmail.com>
2018-07-11 15:33:22 -0400

    Works on the large datasets

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
+
+# python ./src/pharmacy_counting.py ./input/de_cc_data.txt ./output/top_cost_drug.txt
--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -2,43 +2,59 @@ import sys
 
 input_fname, output_fname = sys.argv[1:]
 
+# Keeps record of total cost for each drug
 drug_cost = dict()
-doc_names = dict(list())
+
+# Keeps record of unique doctor names for each drug
+doc_names = dict(set())
+
+n_lines = 0
 
 with open(input_fname, 'rb') as inp_f:
 
         prescription = inp_f.readline()
         prescription = inp_f.readline()
-        # print(dir(prescription))
+
         while len(prescription)>0:
-            print(prescription)
+            n_lines +=1
+            if n_lines%10**6==0:
+                print(n_lines)
+
             prescription = prescription.split(b',' )
             doctor = b' '.join( prescription[1:3] )
 
-            if prescription[3] in drug_cost.keys():
-                if doctor not in doc_names[ prescription[3] ]:
-                    doc_names[ prescription[3] ].append(doctor)
-
-                drug_cost[ prescription[3] ] += int(prescription[-1])
-            else:
-                doc_names[ prescription[3] ] = [doctor]
-                drug_cost[ prescription[3] ] = int(prescription[-1])
+            try:
+                doc_names[ prescription[3] ].add(doctor)
+                drug_cost[ prescription[3] ] += float(prescription[-1])
+            except KeyError:
+                doc_names[ prescription[3] ] = {doctor}
+                drug_cost[ prescription[3] ] = float(prescription[-1])
 
             prescription = inp_f.readline()
 
+# Write the output file
 with open(output_fname, 'wb') as out_f:
+    # Write header of the file
     out_f.write(b'drug_name,num_prescriber,total_cost\n')
 
+    # Sort the keys by the values of drug_cost first
     for drug in sorted(drug_cost, key=drug_cost.get, reverse=True):
+
+        # Write to the output file in descending order
         next_line = b','.join( [drug, bytes( str(len(doc_names[drug])) , 'utf8' ),
-                                bytes( str(drug_cost[drug]) , 'utf8' ) ] )
+                                bytes( str( int(drug_cost[drug])) , 'utf8' ) ] )
         next_line +=b'\n'
         out_f.write(next_line)
-#
-# with open('../output/top_cost_drug.txt', 'rb') as out_f:
-#
-#     a_line = out_f.readline()
-#
-#     while len(a_line)>0:
-#         print(a_line)
-#         a_line = out_f.readline()
+
+
+# Open and print top 10 lines of the output file
+
+print('\nTop 5 lines of the output file\n')
+with open(output_fname, 'rb') as out_f:
+
+    a_line = out_f.readline()
+    n_lines = 0
+    while len(a_line)>0 and n_lines<6:
+        n_lines +=1
+        print(a_line)
+        a_line = out_f.readline()
